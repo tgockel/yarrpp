cf16c24b545de332ddeff7d684077a8e9238f156
Travis Gockel <travis@gockelhut.com>
2017-07-08 21:33:49 -0600

    random.random_fill: Add support for Linux version pre-3.17.

--- src/yarr/random.cpp
+++ src/yarr/random.cpp
@@ -1,14 +1,20 @@
 #include "random.hpp"
 
+#include <yarr/algo/at_each_aligned.hpp>
+
 #include <cerrno>
+#include <cstdint>
 #include <system_error>
 
+#include <linux/version.h>
 #include <linux/random.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
 namespace yarr
 {
+
+#if LINUX_VERSION_CODE > KERNEL_VERSION(3, 17, 0)
 
 static long getrandom(void* buf, std::size_t buf_len, unsigned int flags)
 {
@@ -35,5 +41,23 @@ void random_fill(char* begin, char* end)
         }
     }
 }
+
+#else // older versions of Linux
+
+void random_fill(char* begin, char* end)
+{
+    std::random_device rng;
+    std::uniform_int_distribution<std::uint32_t> dist;
+
+    at_each_aligned<std::uint32_t, std::uint16_t, std::uint8_t>
+    (
+        begin, end,
+        [&] (std::uint32_t* p) { *p = dist(rng); },
+        [&] (std::uint16_t* p) { *p = dist(rng); },
+        [&] (std::uint8_t* p)  { *p = dist(rng); }
+    );
+}
+
+#endif
 
 }
